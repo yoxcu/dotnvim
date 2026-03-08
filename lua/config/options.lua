dda20bfaaa48adcf9eb43616c6862bcea86ab4b4
Michael Auer <mick@yoxcu.de>
2026-03-08 21:35:46 +0100

    mobile

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,35 @@
 --
 vim.opt.exrc = true -- allow local .nvim.lua .vimrc .exrc files
 vim.opt.secure = true -- disable shell and write commands in local .nvim.lua .vimrc .exrc files
+-- Mobile-focused UX defaults
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.showbreak = "↪ "
+
+-- Keep the viewport stable on small screens
+vim.opt.scrolloff = 2
+vim.opt.sidescrolloff = 2
+
+-- Numbers: on narrow screens, relative numbers often feel noisy
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.signcolumn = "yes:1" -- don't reserve a fat sign column
+
+-- Make splits behave predictably on a phone
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Faster/cleaner UI
+vim.opt.cmdheight = 1
+vim.opt.pumheight = 8 -- completion menu height (small!)
+vim.opt.updatetime = 200
+vim.opt.timeoutlen = 400
+
+-- Good for touch / less accidental mode switches
+vim.opt.mouse = "a"
+
+-- Reduce startup noise
+vim.opt.shortmess:append("c")
+vim.opt.shortmess:append("I")
+
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,6 +5,15 @@ return {
       verible = {
         cmd = { "verible-verilog-ls", "--rules_config_search", "--indentation_spaces=4" },
       },
+      clangd = {
+        mason = false,
+      },
+      stylua = {
+        mason = false,
+      },
+      lua_ls = {
+        mason = false,
+      },
     },
   },
 }
